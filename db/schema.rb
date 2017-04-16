a3e8dfa8c32fbb799ff6224a2f6aca43f60826f5
dubroe <elan.dubrofsky@gmail.com>
2017-04-16 18:37:59 -0400

    Seed all of the players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416214517) do
+ActiveRecord::Schema.define(version: 20170416223029) do
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
