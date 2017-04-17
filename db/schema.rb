db1ba0cc8dff02c1347250fc50b24d293c7b60e5
dubroe <elan.dubrofsky@gmail.com>
2017-04-16 23:48:03 -0400

    Switch from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170416224515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string "name"
   end
